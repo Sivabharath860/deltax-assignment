bf67773aaba7c5774c9acf1cc2b04cf35d824302
Siva bharath <sivabharathboppe9059@gmail.com>
2022-07-27 18:36:56 +0530

    Update user.controller.js

--- dx_backend/controllers/user.controller.js
+++ dx_backend/controllers/user.controller.js
@@ -1,26 +1,29 @@
 var connection = require('../config/db_config');
 
-exports.addRating = async(req, res)=>{
+exports.addUser = async(req, res)=>{
     const{songId,userId,rating} = req.body;
-    let sql = "INSERT INTO  spotify.song_rating (songId,userId,rating) VALUES (?,?,?)";
-    connection.query(sql, [songId,userId,rating], function(err, result) {
+    let sql = "INSERT INTO  spotify.users (email,password,userName) VALUES (?,?,?)";
+    connection.query(sql, [email,password,userName], function(err, result) {
         if(err){
             console.log(err);
         }
-        console.log('inserted 88 data');
-        res.json(result);
+        //console.log('inserted 88 data');
+        res.send("inserted");
         //  console.log("query ", sql2);
 
     });
 }
 
-exports.getRatingsByUser = async(req, res)=>{
-    const{userId} = req.body;
-    let sql = "SELECT * FROM spotify.song_rating WHERE userId = ?;";
-    connection.query(sql, [userId], function(err, result) {
+exports.authUser = async(req, res)=>{
+    const{userName,password} = req.body;
+    let sql = "SELECT * FROM users WHERE userName = ? AND password = ?";
+    connection.query(sql, [userName,password], function(err, result) {
         if(err){
             console.log(err);
         }
+        //console.log('inserted 88 data');
         res.json(result);
+        //  console.log("query ", sql2);
+
     });
 }
