cc150775a717240ca7a169fdba1dd971f3267879
Siva bharath <sivabharathboppe9059@gmail.com>
2022-07-27 18:35:28 +0530

    Update artist_song.controller.js

--- dx_backend/controllers/artist_song.controller.js
+++ dx_backend/controllers/artist_song.controller.js
@@ -3,12 +3,13 @@ var connection = require('../config/db_config');
 exports.addComb = async(req, res)=>{
     const{artistId, songId} = req.body;
     let sql = "INSERT INTO  spotify.artist_song (artistId,songId) VALUES (?,?)";
-    connection.query(sql, [artistId,songId], function(err, result) {
+    connection.query(sql, [songId,artistId], function(err, result) {
         if(err){
             console.log(err);
+            res.json(err);
         }
         //console.log('inserted 88 data');
-        res.send("inserted");
+        res.json(result);
         //  console.log("query ", sql2);
 
     });
