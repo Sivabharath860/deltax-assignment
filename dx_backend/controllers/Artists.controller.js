786e7b532b0abaec7bd95c234f41c6729ea8c31e
Siva bharath <sivabharathboppe9059@gmail.com>
2022-07-27 18:34:36 +0530

    Update Artists.controller.js

--- dx_backend/controllers/Artists.controller.js
+++ dx_backend/controllers/Artists.controller.js
@@ -11,7 +11,7 @@ exports.allArtists = async(req, res)=>{
 }
 
 exports.topTenArtists = async(req, res)=>{
-    var query = 'SELECT a.name,AVG(d.avg_rating) FROM artists AS a INNER JOIN spotify.artist_song as a_s ON a.artistId = a_s.artistId JOIN(SELECT songId, AVG(sr.rating) AS avg_rating FROM spotify.song_rating as sr GROUP BY sr.songId ORDER BY AVG(sr.rating) DESC)d ON a_s.songId IN (d.songId)  GROUP BY a.name ORDER BY d.avg_rating DESC LIMIT 10;';
+    var query = 'SELECT a.name,a.dob,a.bio,AVG(d.avg_rating) AS avg_rating FROM artists AS a INNER JOIN spotify.artist_song as a_s ON a.artistId = a_s.artistId JOIN(SELECT songId, AVG(sr.rating) AS avg_rating FROM spotify.song_rating as sr GROUP BY sr.songId ORDER BY AVG(sr.rating) DESC)d ON a_s.songId IN (d.songId)  GROUP BY a.name ORDER BY d.avg_rating DESC LIMIT 10;';
     connection.query(query ,function (error, results, fields) {
         if (error) throw error;
         //console.log(results);
@@ -47,3 +47,25 @@ exports.getArtistCount = async(req, res)=>{
         res.json(results);
     });
 }
+
+exports.autoSuggestArtistName = async(req, res)=>{
+    const {value} = req.body;
+    var query = "SELECT name FROM artists WHERE name LIKE '"+value+"%';";
+    connection.query(query ,[value],function (error, results, fields) {
+        if (error) throw error;
+        console.log(results);
+        res.json(results);
+    });
+}
+
+exports.checkArtist = async(req, res)=>{
+    const {name} = req.body;
+    var query = "SELECT name,artistId FROM artists WHERE name LIKE '"+name+"%';";
+    connection.query(query ,[name],function (error, results, fields) {
+        if (error) throw error;
+        console.log(results);
+        res.json(results)
+    });
+}
+
+
