da340676fd2bad0bfe87d082bee9b01f536f4476
Siva bharath <sivabharathboppe9059@gmail.com>
2022-07-27 18:35:58 +0530

    Update songRating.controller.js

--- dx_backend/controllers/songRating.controller.js
+++ dx_backend/controllers/songRating.controller.js
@@ -7,9 +7,20 @@ exports.addRating = async(req, res)=>{
         if(err){
             console.log(err);
         }
-        //console.log('inserted 88 data');
-        res.send("inserted");
+        console.log('inserted 88 data');
+        res.json(result);
         //  console.log("query ", sql2);
 
+    });
+}
+
+exports.getRatingsByUser = async(req, res)=>{
+    const{userId} = req.body;
+    let sql = "SELECT * FROM spotify.song_rating WHERE userId = ?;";
+    connection.query(sql, [userId], function(err, result) {
+        if(err){
+            console.log(err);
+        }
+        res.json(result);
     });
 }
