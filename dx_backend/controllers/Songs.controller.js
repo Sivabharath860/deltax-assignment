234744f356b36ae5320c72e9abb58adbb81eb192
Siva bharath <sivabharathboppe9059@gmail.com>
2022-07-27 18:35:04 +0530

    Update Songs.controller.js

--- dx_backend/controllers/Songs.controller.js
+++ dx_backend/controllers/Songs.controller.js
@@ -25,7 +25,7 @@ exports.allSongs = async(req, res)=>{
 }
 
 exports.topTenSongs = async(req, res)=>{
-    var query = "SELECT d.sname,d.sid,d.average_rating, a.name FROM spotify.artists AS a INNER JOIN spotify.artist_song as a_s ON a.artistId = a_s.artistId RIGHT JOIN(SELECT s.songName AS sname, sr.songId AS sid, AVG(sr.rating) AS average_rating FROM spotify.song_rating AS sr INNER JOIN spotify.songs AS s ON sr.songId = s.songId GROUP BY sr.songId ORDER BY 3 DESC LIMIT 10)d ON a_s.songId = d.sid ORDER BY 3 DESC;";
+    var query = "SELECT d.sname,d.sid,d.average_rating, a.name, d.image, d.imgName FROM spotify.artists AS a INNER JOIN spotify.artist_song as a_s ON a.artistId = a_s.artistId RIGHT JOIN(SELECT s.songName AS sname, sr.songId AS sid, AVG(sr.rating) AS average_rating, s.image AS image, s.image_name as imgName FROM spotify.song_rating AS sr INNER JOIN spotify.songs AS s ON sr.songId = s.songId GROUP BY sr.songId ORDER BY 3 DESC LIMIT 10)d ON a_s.songId = d.sid ORDER BY 3 DESC;";
     connection.query(query ,function (error, results, fields) {
         if (error) throw error;
         //console.log(results);
@@ -34,24 +34,29 @@ exports.topTenSongs = async(req, res)=>{
 }
 
 exports.addSong = async(req, res)=>{
-    if (!req.files) {
+    //console.log(req);
+    // console.log("...........................");
+    //console.log(req.file);
+    if (!req.files.file) {
         //console.log(req);
         res.send("No file upload");
     } else {
-        console.log(req.body);
-        const{songName, dor} = req.body;
-        console.log(req.files.image.data);
+        //console.log(req)
+        //console.log(req.body);
+        const{name, dor, aname} = req.body;
+        var sid = "";
+        var aid = "";
+        console.log(aname[0].artistId);
+        console.log(req.files.file.data);
         let sql = "INSERT INTO  spotify.songs (songName,dor,image) VALUES (?,?,?)";
-        console.log("hdsfh");
-        connection.query(sql, [songName,dor,req.files.image.data], function(err, result) {
+        connection.query(sql, [name,dor,req.files.file.data], function(err, result) {
             if(err){
                 console.log(err);
             }
             console.log('inserted 88 data');
-            res.send("inserted");
-            //  console.log("query ", sql2);
-
+            res.json(result);
         });
+        
     }
 }
 
@@ -60,6 +65,17 @@ exports.getCount = async(req, res)=>{
     connection.query(query ,function (error, results, fields) {
         if (error) throw error;
         //console.log(results);
+        res.json(results);
+    });
+}
+
+exports.checkSong = async(req, res)=>{
+    const {value} = req.body;
+    console.log(value);
+    var query = "SELECT songName,songId FROM songs WHERE songName LIKE '"+value+"%';";
+    connection.query(query ,function (error, results, fields) {
+        if (error) throw error;
+        console.log(results);
         res.json(results);
     });
 }
